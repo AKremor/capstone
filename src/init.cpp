37adfa3da295e4c3bf0aac4cab6b631960d6021a
Anthony Kremor <github@anthonykremor.com>
2018-08-05 17:48:01 +1000

    Remove RTSC warning and separate SVM/staircase timers

--- src/init.cpp
+++ src/init.cpp
@@ -13,6 +13,8 @@ void setPinsBuffered(uint32_t ui32Port, uint8_t ui8Pins, uint8_t ui8Val);
 FILE *fp;
 void timerCallback(Timer_Handle handle);
 
+void stair_case_timer_callback(Timer_Handle handle);
+
 volatile uint64_t state_counter = 0;
 
 // PL0 9VL POS
@@ -61,7 +63,6 @@ Timer_Handle timer1;
 
 void *mainThread(void *arg0) {
     start_chopper();
-    // fp = fopen("svm.csv", "w");
 
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOL);
     while (!(SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOL)))
@@ -93,7 +94,7 @@ void *mainThread(void *arg0) {
     params.period = sizeof(states) * svm_frequency_hz;
     params.periodUnits = Timer_PERIOD_HZ;
     params.timerMode = Timer_CONTINUOUS_CALLBACK;
-    params.timerCallback = timerCallback;
+    params.timerCallback = stair_case_timer_callback;
 
     timer0 = Timer_open(Board_TIMER0, &params);
 
@@ -137,7 +138,7 @@ void applyPortSetting(uint32_t ui32Port) {
     GPIOPinWrite(ui32Port, 0xFF, port_buffer[port_buffer_index]);
 }
 
-void timerCallback(Timer_Handle myHandle) {
+void stair_case_timer_callback(Timer_Handle myHandle) {
     /*float32_t Vdc = 1;
     float32_t time = Timer_getCount(timer1);
     abc_quantity value = SineWave::getValueAbc(state_counter);
@@ -296,6 +297,156 @@ void timerCallback(Timer_Handle myHandle) {
     setPinsBuffered(GPIO_PORTL_BASE, states[state_counter % sizeof(states)],
                     true);
     applyPortSetting(GPIO_PORTL_BASE);
+
+    state_counter++;
+}
+
+void svm_timer_callback(Timer_Handle myHandle) {
+    float32_t Vdc = 1;
+    float32_t time = Timer_getCount(timer1);
+    // TODO(akremor): Substitute time in here
+    abc_quantity value = SineWave::getValueAbc(state_counter);
+
+    // TODO(akremor): Need to ensure the output magnitude is scaled
+    // appropriately
+
+    gh_quantity hex_value;
+    hex_value.g = 1 / (3 * Vdc) * (2 * value.a - value.b - value.c);
+    hex_value.h = 1 / (3 * Vdc) * (-1 * value.a + 2 * value.b - value.c);
+
+    gh_quantity Vul = {ceil(hex_value.g), floor(hex_value.h)};
+    gh_quantity Vlu = {floor(hex_value.g), ceil(hex_value.h)};
+    gh_quantity Vuu = {ceil(hex_value.g), ceil(hex_value.h)};
+    gh_quantity Vll = {floor(hex_value.g), floor(hex_value.h)};
+
+    gh_quantity nearest_1 = Vul;
+    gh_quantity nearest_2 = Vlu;
+    gh_quantity nearest_3;
+
+    if (copysignf(1.0, hex_value.g + hex_value.h - Vul.g - Vul.h) == 1) {
+        nearest_3 = Vuu;
+    } else {
+        nearest_3 = Vll;
+    }
+
+    // Now we need to find an available voltage state
+
+    bool all_satisfied = false;
+    int32_t k = -4;
+    int32_t abs_bound = 4;
+    while (!all_satisfied) {
+        if ((-1 * abs_bound <= k && k <= abs_bound) &&
+            (-1 * abs_bound <= k - nearest_1.g &&
+             k - nearest_1.g <= abs_bound) &&
+            (-1 * abs_bound <= k - nearest_1.g - nearest_1.h &&
+             k - nearest_1.g - nearest_1.h <= abs_bound)) {
+            all_satisfied = true;
+        } else {
+            k++;
+        }
+    }
+
+    int32_t a_phase = k;
+    int32_t b_phase = k - nearest_1.g;
+    int32_t c_phase = k - nearest_1.g - nearest_1.h;
+
+    setPinsBuffered(GPIO_PORTC_BASE, 0xFF, false);
+    setPinsBuffered(GPIO_PORTL_BASE, 0xFF, false);
+    setPinsBuffered(GPIO_PORTK_BASE, 0xFF, false);
+
+    switch (a_phase) {
+        case -4:
+            setPinsBuffered(GPIO_PORTL_BASE, A_NEG9 | A_NEG3, true);
+            break;
+        case -3:
+            setPinsBuffered(GPIO_PORTL_BASE, A_NEG9 | A_OFF3, true);
+            break;
+        case -2:
+            setPinsBuffered(GPIO_PORTL_BASE, A_NEG9 | A_POS3, true);
+            break;
+        case -1:
+            setPinsBuffered(GPIO_PORTL_BASE, A_OFF9 | A_POS3, true);
+            break;
+        case 0:
+            setPinsBuffered(GPIO_PORTL_BASE, A_OFF9 | A_OFF3, true);
+            break;
+        case 1:
+            setPinsBuffered(GPIO_PORTL_BASE, A_OFF9 | A_POS3, true);
+            break;
+        case 2:
+            setPinsBuffered(GPIO_PORTL_BASE, A_POS9 | A_NEG3, true);
+            break;
+        case 3:
+            setPinsBuffered(GPIO_PORTL_BASE, A_POS9 | A_OFF3, true);
+            break;
+        case 4:
+            setPinsBuffered(GPIO_PORTL_BASE, A_POS9 | A_POS3, true);
+            break;
+    }
+
+    switch (b_phase) {
+        case -4:
+            setPinsBuffered(GPIO_PORTK_BASE, B_NEG9 | B_NEG3, true);
+            break;
+        case -3:
+            setPinsBuffered(GPIO_PORTK_BASE, B_NEG9 | B_OFF3, true);
+            break;
+        case -2:
+            setPinsBuffered(GPIO_PORTK_BASE, B_NEG9 | B_POS3, true);
+            break;
+        case -1:
+            setPinsBuffered(GPIO_PORTK_BASE, B_OFF9 | B_POS3, true);
+            break;
+        case 0:
+            setPinsBuffered(GPIO_PORTK_BASE, B_OFF9 | B_OFF3, true);
+            break;
+        case 1:
+            setPinsBuffered(GPIO_PORTK_BASE, B_OFF9 | B_POS3, true);
+            break;
+        case 2:
+            setPinsBuffered(GPIO_PORTK_BASE, B_POS9 | B_NEG3, true);
+            break;
+        case 3:
+            setPinsBuffered(GPIO_PORTK_BASE, B_POS9 | B_OFF3, true);
+            break;
+        case 4:
+            setPinsBuffered(GPIO_PORTK_BASE, B_POS9 | B_POS3, true);
+            break;
+    }
+
+    switch (c_phase) {
+        case -4:
+            setPinsBuffered(GPIO_PORTC_BASE, C_NEG9 | C_NEG3, true);
+            break;
+        case -3:
+            setPinsBuffered(GPIO_PORTC_BASE, C_NEG9 | C_OFF3, true);
+            break;
+        case -2:
+            setPinsBuffered(GPIO_PORTC_BASE, C_NEG9 | C_POS3, true);
+            break;
+        case -1:
+            setPinsBuffered(GPIO_PORTC_BASE, C_OFF9 | C_POS3, true);
+            break;
+        case 0:
+            setPinsBuffered(GPIO_PORTC_BASE, C_OFF9 | C_OFF3, true);
+            break;
+        case 1:
+            setPinsBuffered(GPIO_PORTC_BASE, C_OFF9 | C_POS3, true);
+            break;
+        case 2:
+            setPinsBuffered(GPIO_PORTC_BASE, C_POS9 | C_NEG3, true);
+            break;
+        case 3:
+            setPinsBuffered(GPIO_PORTC_BASE, C_POS9 | C_OFF3, true);
+            break;
+        case 4:
+            setPinsBuffered(GPIO_PORTC_BASE, C_POS9 | C_POS3, true);
+            break;
+    }
+
+    applyPortSetting(GPIO_PORTC_BASE);
+    applyPortSetting(GPIO_PORTL_BASE);
+    applyPortSetting(GPIO_PORTK_BASE);
 
     state_counter++;
 }
