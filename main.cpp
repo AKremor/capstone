15b65fda13064554b77aedebb8178bd7d1dd93f8
Anthony Kremor <github@anthonykremor.com>
2018-08-10 18:47:39 +1000

    Switch to TI RTOS task creation

--- main.cpp
+++ main.cpp
@@ -1,60 +1,15 @@
-#include <pthread.h>
-#include <stdint.h>
 #include <ti/sysbios/BIOS.h>
-
-#include <src/hal/Board.h>
+#include <ti/sysbios/knl/Task.h>
 
 extern void *mainThread(void *arg0);
 
-/* Stack size in bytes */
-#define THREADSTACKSIZE 5024
-
-// TI-RTOS expects to find a definition at link time. We don't use networking
-// functionality therefore give it an empty definition.
-extern "C" {
-void NDK_hookInit(int32_t id) {}
-}
-
-/*
- *  ======== main ========
- */
 int main(void) {
-    pthread_t thread;
-    pthread_attr_t attrs;
-    struct sched_param priParam;
-    int retc;
-    int detachState;
-
-    /* Call driver init functions */
-    Board_initGeneral();
-
-    /* Set priority and stack size attributes */
-    pthread_attr_init(&attrs);
-    priParam.sched_priority = 1;
-
-    detachState = PTHREAD_CREATE_DETACHED;
-    retc = pthread_attr_setdetachstate(&attrs, detachState);
-    if (retc != 0) {
-        /* pthread_attr_setdetachstate() failed */
-        while (1)
-            ;
-    }
-
-    pthread_attr_setschedparam(&attrs, &priParam);
-
-    retc |= pthread_attr_setstacksize(&attrs, THREADSTACKSIZE);
-    if (retc != 0) {
-        /* pthread_attr_setstacksize() failed */
-        while (1)
-            ;
-    }
-
-    retc = pthread_create(&thread, &attrs, mainThread, NULL);
-    if (retc != 0) {
-        /* pthread_create() failed */
-        while (1)
-            ;
-    }
+    Task_Params task_params;
+    Task_Params_init(&task_params);
+    task_params.stackSize = 1024;
+    task_params.instance->name = "Main thread";
+    task_params.priority = 1;
+    Task_create((Task_FuncPtr)mainThread, &task_params, NULL);
 
     BIOS_start();
 
